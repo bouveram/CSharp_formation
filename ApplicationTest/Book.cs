51a724c484c9e7542b71b92dc1d2f271c13afd6c
bouveram <maximebouverat@gmail.com>
2015-10-08 10:12:36 +0200

    First Commit with VS2015

---
+++ ApplicationTest/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTest
+{
+    public class Book
+    {
+        private string title;
+        private int id;
+        private int nbPage;
+        private string isbn;
+        private double price;
+        private string lang;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public int Id { get; set; }
+
+        public int NbPage { get; set; }
+
+        public string Isbn { get; set; }
+
+        public double Price { get; set; }
+
+        public string Lang { get; set; }
+    }
+}
---
+++ ApplicationTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book test = new Book();
+
+            test.Title = "ceci est le titre de mon livre";
+
+            Console.WriteLine(test.Title);
+            Console.ReadKey();
+        }
+    }
+
+}
